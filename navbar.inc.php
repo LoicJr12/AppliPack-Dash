f09ba8b673a3ce635f276a5dbc406fae986629f8
KAKOU <goue.kakou@groupe-esigelec.org>
2025-10-06 12:42:17 +0200

    Création du contenu de la page d'accueil (header, navbar)

---
+++ navbar.inc.php
@@ -0,0 +1,28 @@
+<nav class = "navbar navbar-expand-lg  menu" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+        <a class="navbar-brand titleMenu" href="#">
+            <img src="assets/removebg-logo.png" alt="logo Pack & Dash" width="60" height="40" class="d-inline-block align-text-bottom">
+            Pack & Dash
+        </a>
+        <div class="listLink">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Infos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">A propos de nous</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacter nous</a>
+                </li>
+            </ul>
+        </div>
+        <div class="listButton">
+            <button type="button" class="btn btn-success">Se connecter</button>
+            <button type="button" class="btn btn-primary">S'inscrire</button>
+        </div>
+    </div>
+</nav>
