5308a78eec81ba076b123d422449e7fdfe212b8d
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-20 14:46:26 +0200

    mise à jour de la barre de navigation

--- navbar.inc.php
+++ navbar.inc.php
@@ -1,13 +1,13 @@
-<nav class = "navbar navbar-expand-lg  menu" style="background-color: #e3f2fd;">
+<nav class = "navbar navbar-expand-lg">
     <div class="container-fluid">
-        <a class="navbar-brand titleMenu" href="#">
+        <a class="navbar-brand titleNavbar" href="#">
             <img src="assets/removebg-logo.png" alt="logo Pack & Dash" width="60" height="40" class="d-inline-block align-text-bottom">
             Pack & Dash
         </a>
         <div class="listLink">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    <a class="nav-link" aria-current="page" href="#">Accueil</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Infos</a>
@@ -20,9 +20,45 @@
                 </li>
             </ul>
         </div>
-        <div class="listButton">
-            <button type="button" class="btn btn-success">Se connecter</button>
-            <button type="button" class="btn btn-primary">S'inscrire</button>
+        <div class="listButton list-Buttons">
+            <a class="nav-link link-connection" href="#">Se connecter</a>
+            <a class="nav-link link-inscription" href="#">S'inscrire</a>
+            <div class="bar-diviseur-vertical"></div>
+            <i class="fa-solid fa-cloud-moon"></i>
+        </div>
+        <div class="menu-icon">
+            <i class="fa-solid fa-bars"></i>
+            <span>Menu</span>
         </div>
     </div>
 </nav>
+
+<div class="menu-responsive">
+    <div class="listLink-menu">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Infos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">A propos de nous</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contacter nous</a>
+            </li>
+        </ul>
+    </div>
+    <div class="bar-diviseur-horizontal"></div>
+    <div class="list-Buttons-menu">
+        <a class="nav-link link-connection" href="#">Se connecter</a>
+        <a class="nav-link link-inscription" href="#">S'inscrire</a>
+        <a class="nav-link link-dark-mode bg-dark" href="#">
+            <span>Dark mode</span>
+            <i class="fa-solid fa-moon"></i>
+        </a>
+    </div>
+
+</div>
+
