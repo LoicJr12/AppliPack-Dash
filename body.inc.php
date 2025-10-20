454e3e814b1a4e92dd459bd726af00dde448a93e
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-20 15:17:50 +0200

    mise à jour du body et création de son style

--- body.inc.php
+++ body.inc.php
@@ -1,23 +1,13 @@
 <div class="containeur-fluid main-content">
     <div class="row">
-        <div class="col-5">
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium <br>
-                itaque facilis eligendi optio inventore sequi incidunt, deserunt neque sed <br>
-                culpa quia voluptas exercitationem alias quis architecto dolore temporibus modi.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium <br>
-                itaque facilis eligendi optio inventore sequi incidunt, deserunt neque sed <br>
-                culpa quia voluptas exercitationem alias quis architecto dolore temporibus modi.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium <br>
-                itaque facilis eligendi optio inventore sequi incidunt, deserunt neque sed <br>
-                culpa quia voluptas exercitationem alias quis architecto dolore temporibus modi.
-            </p>
+        <div class="col-4">
+            <h5 class="text-slogan">
+                Chez <h5 class="title">Pack & Dash</h5>
+                Nous croyons que votre déménagement ne derait pas être une source de stress et<br>
+                devrait se faire en toute sérénité. <h5 lass="slogan">Vote déménagement notre priorité</h5>.
+            </h5>
         </div>
-        <div class="col-7">
+        <div class="col-8">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="2000">
