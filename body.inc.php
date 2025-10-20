82806ce33c4212d9d9d1a4889a0a4c8d458d119c
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-20 16:30:42 +0200

    mise à jour du body et de son style

--- body.inc.php
+++ body.inc.php
@@ -1,23 +1,46 @@
 <div class="containeur-fluid main-content">
     <div class="row">
-        <div class="col-4">
-            <h5 class="text-slogan">
-                Chez <h5 class="title">Pack & Dash</h5>
-                Nous croyons que votre déménagement ne derait pas être une source de stress et<br>
-                devrait se faire en toute sérénité. <h5 lass="slogan">Vote déménagement notre priorité</h5>.
-            </h5>
+        <div class="col-lg-5 bloc-text">
+            <p class="text-slogan">
+                Chez <span class="title">Pack & Dash</span>,<br>
+                Nous croyons que votre déménagement ne derait pas être une 
+                source de stress et devrait se faire en toute sérénité.
+                <span class="slogan">Vote déménagement notre priorité</span>.
+            </p>
+            <div class = "description">
+                <h5 class="text">Comment ça marche</h5>
+                <div class="instruction">
+                    <span class="badge text-bg-primary border border-light rounded-circle">1</span>
+                    Publier une annonce
+                </div>
+                <div class="instruction">
+                    <span class="badge text-bg-primary border border-light rounded-circle">2</span>
+                    Recever des propositions
+                </div>
+                <div class="instruction">
+                    <span class="badge text-bg-primary border border-light rounded-circle">3</span>
+                    Choisissez votre déménageur
+                </div>
+                <div class="instruction">
+                    <span class="badge text-bg-primary border border-light rounded-circle">4</span>
+                    Notez la prestation du déménageur
+                </div>
+            </div>
+            <div class="avis-clients">
+                <h5 class="text">Avis Clients</h5>
+            </div>
         </div>
-        <div class="col-8">
+        <div class="col-lg-7">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="2000">
-                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 1">
+                        <img src="assets/image_body.png" class="d-block w-100 img-fluid" alt="img body 1">
                     </div>
                     <div class="carousel-item" data-bs-interval="2000">
-                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 2">
+                        <img src="assets/image_body.png" class="d-block w-100 img-fluid" alt="img body 2">
                     </div>
                     <div class="carousel-item" data-bs-interval="2000">
-                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 3">
+                        <img src="assets/image_body.png" class="d-block w-100 img-fluid" alt="img body 3">
                     </div>
                 </div>
             </div>
