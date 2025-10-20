55da755284c89c090cb1bfdfb8efac833da413c1
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-20 14:56:43 +0200

    création du contenu principal de la page d'accueil

---
+++ body.inc.php
@@ -0,0 +1,36 @@
+<div class="containeur-fluid main-content">
+    <div class="row">
+        <div class="col-5">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium <br>
+                itaque facilis eligendi optio inventore sequi incidunt, deserunt neque sed <br>
+                culpa quia voluptas exercitationem alias quis architecto dolore temporibus modi.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium <br>
+                itaque facilis eligendi optio inventore sequi incidunt, deserunt neque sed <br>
+                culpa quia voluptas exercitationem alias quis architecto dolore temporibus modi.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laudantium <br>
+                itaque facilis eligendi optio inventore sequi incidunt, deserunt neque sed <br>
+                culpa quia voluptas exercitationem alias quis architecto dolore temporibus modi.
+            </p>
+        </div>
+        <div class="col-7">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="2000">
+                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 1">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 2">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 3">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
