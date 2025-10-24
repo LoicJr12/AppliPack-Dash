3738e18de09100341d0a43a014545bfbf19e5bb4
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-24 12:20:48 +0200

    mise à jour du bas de la page (footer)

--- footer.inc.php
+++ footer.inc.php
@@ -1,10 +1,42 @@
 <footer>
     <div class="container-fluid">
         <div class="social-media">
-
+            <span>
+                <i class="fa-brands fa-x-twitter fa-2xl" style="color: #74C0FC;"></i>
+                #X-Pack&Dash
+            </span>
+            <span>
+                <i class="fa-brands fa-facebook-f fa-2xl" style="color: #74C0FC;"></i>
+                #F-Pack&Dash
+            </span>
+            <span>
+                <i class="fa-brands fa-instagram fa-2xl" style="color: #74C0FC;"></i>
+                #I-Pack&Dash
+            </span>
+            <span>
+                <i class="fa-brands fa-whatsapp fa-2xl" style="color: #74C0FC;"></i>
+                #I-Pack&Dash
+            </span>
         </div>
+        <div class="bar"></div>
         <div class="adresses">
-                
+            <span>
+                <i class="fa-solid fa-map-pin" style="color: #74C0FC;"></i>
+                2 Rue Henri Wallon, Saint-Etienne-du-Rouvray
+            </span>
+            <span>
+                <i class="fa-solid fa-phone fa-2xl" style="color: #74C0FC;"></i>
+                +33 (0) 7-85-98-45
+            </span>
+            <span>
+                <i class="fa-regular fa-envelope fa-2xl" style="color: #74C0FC;"></i>
+                site@gmail.com
+            </span>
+            <span>
+                <i class="fa-regular fa-envelope fa-2xl" style="color: #74C0FC;"></i>
+                site@gmail.com
+            </span>
+
         </div>
     </div>
 </footer>
