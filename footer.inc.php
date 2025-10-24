72977967e756915377ca0c195a33b0fdcd7e027d
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-24 11:59:23 +0200

    creation du bas de pages et de son style (footer)

---
+++ footer.inc.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container-fluid">
+        <div class="social-media">
+
+        </div>
+        <div class="adresses">
+                
+        </div>
+    </div>
+</footer>
