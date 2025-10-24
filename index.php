d664474092c5960525271af5d957e12e3d3f41c3
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-24 12:23:19 +0200

    ajout du footer à la page d'accueil

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <?php
         include('navbar.inc.php');
         include('body.inc.php');
+        include('footer.inc.php');
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script src="styles/animation.menu.js"></script>
