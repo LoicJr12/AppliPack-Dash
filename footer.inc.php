a6c56e499ea69f81a7799200d82a88e85b0bb9f0
LoicJr12 <goue.kakou@groupe-esigelec.org>
2025-10-24 12:30:23 +0200

    mise à jour du style du footer et ajouter du style à la page d'accueil

--- footer.inc.php
+++ footer.inc.php
@@ -2,19 +2,19 @@
     <div class="container-fluid footer">
         <div class="social-media">
             <span>
-                <i class="fa-brands fa-x-twitter fa-2xl" style="color: #74C0FC;"></i>
+                <i class="fa-brands fa-x-twitter fa-xl" style="color: #74C0FC;"></i>
                 #X-Pack&Dash
             </span>
             <span>
-                <i class="fa-brands fa-facebook-f fa-2xl" style="color: #74C0FC;"></i>
+                <i class="fa-brands fa-facebook-f fa-xl" style="color: #74C0FC;"></i>
                 #F-Pack&Dash
             </span>
             <span>
-                <i class="fa-brands fa-instagram fa-2xl" style="color: #74C0FC;"></i>
+                <i class="fa-brands fa-instagram fa-xl" style="color: #74C0FC;"></i>
                 #I-Pack&Dash
             </span>
             <span>
-                <i class="fa-brands fa-whatsapp fa-2xl" style="color: #74C0FC;"></i>
+                <i class="fa-brands fa-whatsapp fa-xl" style="color: #74C0FC;"></i>
                 #I-Pack&Dash
             </span>
         </div>
@@ -25,15 +25,15 @@
                 2 Rue Henri Wallon, Saint-Etienne-du-Rouvray
             </span>
             <span>
-                <i class="fa-solid fa-phone fa-2xl" style="color: #74C0FC;"></i>
+                <i class="fa-solid fa-phone fa-xl" style="color: #74C0FC;"></i>
                 +33 (0) 7-85-98-45
             </span>
             <span>
-                <i class="fa-regular fa-envelope fa-2xl" style="color: #74C0FC;"></i>
+                <i class="fa-regular fa-envelope fa-xl" style="color: #74C0FC;"></i>
                 site@gmail.com
             </span>
             <span>
-                <i class="fa-regular fa-envelope fa-2xl" style="color: #74C0FC;"></i>
+                <i class="fa-regular fa-envelope fa-xl" style="color: #74C0FC;"></i>
                 site@gmail.com
             </span>
 
