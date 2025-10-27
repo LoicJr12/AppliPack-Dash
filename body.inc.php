9a7a1def38d677c97a1b2b91c4815a4694033a26
Loïc Jr 11 <loicjr77@gmail.com>
2025-10-27 13:26:13 +0100

    retra

--- body.inc.php
+++ body.inc.php
@@ -33,14 +33,14 @@
         <div class="col-lg-7">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="2000">
-                        <img src="assets/image_body.png" class="d-block w-100 img-fluid" alt="img body 1">
+                    <div class="carousel-item active" data-bs-interval="3000">
+                        <img src="assets/image_body.png" class="d-block img-fluid" alt="img body 1">
                     </div>
-                    <div class="carousel-item" data-bs-interval="2000">
-                        <img src="assets/image_body.png" class="d-block w-100 img-fluid" alt="img body 2">
+                    <div class="carousel-item" data-bs-interval="3000">
+                        <img src="assets/image_body.png" class="d-block img-fluid" alt="img body 2">
                     </div>
-                    <div class="carousel-item" data-bs-interval="2000">
-                        <img src="assets/image_body.png" class="d-block w-100 img-fluid" alt="img body 3">
+                    <div class="carousel-item" data-bs-interval="3000">
+                        <img src="assets/image_body.png" class="d-block img-fluid" alt="img body 3">
                     </div>
                 </div>
             </div>
