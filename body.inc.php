5a151ac7878a1c99c143363af84ff12c14477e6a
Loïc Jr 11 <loicjr77@gmail.com>
2025-10-27 14:50:44 +0100

    rajout des avis clients

--- body.inc.php
+++ body.inc.php
@@ -26,9 +26,6 @@
                     Notez la prestation du déménageur
                 </div>
             </div>
-            <div class="avis-clients">
-                <h5 class="text">Avis Clients</h5>
-            </div>
         </div>
         <div class="col-lg-7">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
@@ -44,6 +41,53 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="col avis-clients">
+        <h5 class="text">Avis Clients</h5>
+        <div class="row commentaires">
+            <?php
+                // Tableau contenant les données (images + commentaires)
+                $cards = [
+                    [
+                        "image" => "assets/image_body.png",
+                        "user" => "user-@1234",
+                        "comment" => "je recommande à 100% 😍"
+                    ],
+                    [
+                        "image" => "assets/image_body.png",
+                        "user" => "user-@12345",
+                        "comment" => "c'est le meilleur service de la terre 😆!"
+                    ],
+                    [
+                        "image" => "assets/image_body.png",
+                        "user" => "user-@123450",
+                        "comment" => "Je suis ravie de les avoir connu trop top 😁"
+                    ],
+                    [
+                        "image" => "assets/image_body.png",
+                        "user" => "user-@123459",
+                        "comment" => "En plus d'etre pas cher, ils sont pros 😉!"
+                    ],
+                    [
+                        "image" => "assets/image_body.png",
+                        "user" => "user-@123459",
+                        "comment" => "Wesh, c'est trop un D leur entreprise. Chapeau🫠"
+                    ]
+                ];
+
+                foreach ($cards as $card) {
+                    echo '
+                        <div class="card" style="width: 16rem;">
+                            <img src="'.$card["image"].'" class="card-img-top" alt="img-profile">
+                            <div class="card-body">
+                                <h4 class="card-title">'.$card["user"].'</h4>
+                                <p class="card-text">'.$card["comment"].'</p>
+                            </div>
+                        </div>
+                    ';
+                }
+            ?>
         </div>
     </div>
 </div>
