334b41f5d0ae604ea8a9dae77332096fdfc27706
Loïc Jr 11 <loicjr77@gmail.com>
2025-10-27 16:34:50 +0100

    rajout du copyright dans footee.inc.php et mise à jour du style footer.css

--- footer.inc.php
+++ footer.inc.php
@@ -1,47 +1,46 @@
 <footer>
     <div class="container-fluid footer">
-        <div class="row addr&social">
-            <div class="social-media">
-                <span>
-                    <i class="fa-brands fa-x-twitter fa-xl" style="color: #fff;"></i>
-                    #X-Pack&Dash
-                </span>
-                <span>
-                    <i class="fa-brands fa-facebook-f fa-xl" style="color: #fff;"></i>
-                    #F-Pack&Dash
-                </span>
-                <span>
-                    <i class="fa-brands fa-instagram fa-xl" style="color: #fff;"></i>
-                    #I-Pack&Dash
-                </span>
-                <span>
-                    <i class="fa-brands fa-whatsapp fa-xl" style="color: #fff;"></i>
-                    #W-Pack&Dash
-                </span>
-            </div>
-            <div class="bar"></div>
-            <div class="adresses">
-                <span>
-                    <i class="fa-solid fa-map-pin" style="color: #fff;"></i>
-                    2 Rue Henri Wallon 76800
-                </span>
-                <span>
-                    <i class="fa-solid fa-phone fa-xl" style="color: #fff;"></i>
-                    +33 (0) 7-85-98-45
-                </span>
-                <span>
-                    <i class="fa-regular fa-envelope fa-xl" style="color: #fff;"></i>
-                    site@gmail.com
-                </span>
-                <span>
-                    <i class="fa-regular fa-envelope fa-xl" style="color: #fff;"></i>
-                    site@gmail.com
-                </span>
-            </div>
+        <div class="social-media">
+            <span>
+                <i class="fa-brands fa-x-twitter fa-xl" style="color: #fff;"></i>
+                #X-Pack&Dash
+            </span>
+            <span>
+                <i class="fa-brands fa-facebook-f fa-xl" style="color: #fff;"></i>
+                #F-Pack&Dash
+            </span>
+            <span>
+                <i class="fa-brands fa-instagram fa-xl" style="color: #fff;"></i>
+                #I-Pack&Dash
+            </span>
+            <span>
+                <i class="fa-brands fa-whatsapp fa-xl" style="color: #fff;"></i>
+                #W-Pack&Dash
+            </span>
         </div>
-        <div class="col copyrigt">
-            <span>© Copyright - Tous droits reservés</span>
-            <span>fait par KAKOU & HOUNKPONOU</span>
+        <div class="bar"></div>
+        <div class="adresses">
+            <span>
+                <i class="fa-solid fa-map-pin" style="color: #fff;"></i>
+                2 Rue Henri Wallon, 76800
+            </span>
+            <span>
+                <i class="fa-solid fa-phone fa-xl" style="color: #fff;"></i>
+                +33 (0) 7-85-98-45
+            </span>
+            <span>
+                <i class="fa-solid fa-fax fa-xl" style="color: #fff;"></i>
+                +33 (0) 2-02-12-12
+            </span>
+            <span>
+                <i class="fa-regular fa-envelope fa-xl" style="color: #fff;"></i>
+                site@gmail.com
+            </span>
+
         </div>
+    </div>
+    <div class="container-fluid copyright">
+        <span>© Copyright, Tous droits reservés</span>
+        <span>fait par KAKOU & HOUNKPONOU</span>
     </div>
 </footer>
