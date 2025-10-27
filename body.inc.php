85345660486d017bc8e5ba750b8bc0cb6b4bcc17
Loïc Jr 11 <loicjr77@gmail.com>
2025-10-27 13:50:44 +0100

    retrait de img-fluid dans body.inc.php et imposition d'une hauteur sur l'img du carrousel-iner dans le style

--- body.inc.php
+++ body.inc.php
@@ -34,13 +34,13 @@
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="3000">
-                        <img src="assets/image_body.png" class="d-block img-fluid" alt="img body 1">
+                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 1">
                     </div>
                     <div class="carousel-item" data-bs-interval="3000">
-                        <img src="assets/image_body.png" class="d-block img-fluid" alt="img body 2">
+                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 2">
                     </div>
                     <div class="carousel-item" data-bs-interval="3000">
-                        <img src="assets/image_body.png" class="d-block img-fluid" alt="img body 3">
+                        <img src="assets/image_body.png" class="d-block w-100" alt="img body 3">
                     </div>
                 </div>
             </div>
