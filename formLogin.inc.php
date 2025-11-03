66f6c629d14dcb824eb23ad6cbfcad8cf8f40e6c
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-03 11:09:01 +0100

    creation du formulaire de login et rajout d'une image login

---
+++ formLogin.inc.php
@@ -0,0 +1,10 @@
+<main>
+    <div class="row container-fluid">
+        <div class="col-lg-6">
+    
+        </div>
+        <div class="col-lg-6">
+            <img src="img-fluid" alt="image login authentification" srcset="">
+        </div>
+    </div>
+</main>
