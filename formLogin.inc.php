7eea1327460492833f05fdd77b0d140f4e4f17f4
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-03 11:32:39 +0100

    update formulaire login

--- formLogin.inc.php
+++ formLogin.inc.php
@@ -1,10 +1,38 @@
 <main>
     <div class="row container-fluid">
         <div class="col-lg-6">
-    
+            <div class="row titre">
+                <img src="assets/removebg-logo.png" alt="logo Pack & Dash" width="60" height="40" class="d-inline-block align-text-bottom">
+                Pack & Dash
+            </div>
+            <div class="col authentification">
+                <div>
+                    <h3>AUTHENTIFICATION</h3>
+                    <span>Entrer vos détails de connexion</span>
+                </div>
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <div class="buttons-form">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="reset" class="btn btn-danger">reset</button>
+                    </div>
+                </form>
+            </div>
         </div>
         <div class="col-lg-6">
-            <img src="img-fluid" alt="image login authentification" srcset="">
+            <img src="assets/imageLogin.jpg" class="w-100 img-fluid" alt="image login authentification" >
         </div>
     </div>
 </main>
