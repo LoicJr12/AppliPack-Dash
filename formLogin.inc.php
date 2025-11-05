2cffc5d808032884b70dfe82bfd9a41a434ccc33
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-06 00:38:45 +0100

    redirection vers traitementLogin pour le traitement du formulaire

--- formLogin.inc.php
+++ formLogin.inc.php
@@ -9,19 +9,22 @@
                 <div class="auth-img">
                     <img src="assets/imageLogin2.jpg" alt="image login 2">
                 </div>
-                <form action="" method="post" class="formulaire">
+                <form action="traitementLogin.php" method="POST" class="formulaire">
                     <div class="mb-3">
                         <h3>AUTHENTIFICATION</h3>
                         <span class="text-body-secondary">Entrer vos détails de connexion</span>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="xxxxx@hotmail.com">
+                        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="xxxxx@hotmail.com" required>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
+                        <div id="passwordHelpBlock" class="form-text">
+                            Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                        </div>
                     </div>
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
