053d911b7c58172661996cace8515981b6be1cc5
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-10 13:22:57 +0100

    recuperation de l'erreur apres authentification

--- login.inc.php
+++ login.inc.php
@@ -2,7 +2,13 @@
         <?php
             $title = 'Login Page';
             include('header.inc.php');
-            include('formLogin.inc.php');
+            if (isset($_GET['error']) && $_GET['error'] == 1){
+                $codErreur=1;
+                include('formLogin.inc.php');
+            }else{
+                $codErreur=0;
+                include('formLogin.inc.php');
+            }
             include('footer.inc.php');
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
