be2818a2a10f82038ed1b9fb0fbea42542989328
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-12 17:54:05 +0100

    update

--- demenageur.inc.php
+++ demenageur.inc.php
@@ -1,9 +1,9 @@
         <?php
-            $title = 'Pack & Dash';
+            $title = 'Dashboard Demenageur';
             include('header.inc.php');
-            //include('navbar.demenageur.php');
-            //include('search.demenageur.php');
-            //include('side.demenageur.php');
+            include('navbar.inc.php');
+            include('demenageur/search.demenageur.php');
+            include('demenageur/side.demenageur.php');
             include('footer.inc.php');
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
