8265a721281454ba3ce9fe0757f74b7dab581bb1
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-12 17:53:54 +0100

    rajout des specificités pour la page demenageur

--- navbar.inc.php
+++ navbar.inc.php
@@ -4,6 +4,7 @@
             <img src="assets/removebg-logo.png" alt="logo Pack & Dash" width="60" height="40" class="d-inline-block align-text-bottom">
             Pack & Dash
         </a>
+        <?php if($title !== 'Dashboard Demenageur'): ?>
         <div class="listLink">
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -20,16 +21,32 @@
                 </li>
             </ul>
         </div>
+        <?php endif; ?>
         <div class="listButton list-Buttons">
-            <a class="nav-link link-connection" href="login.inc.php">Se connecter</a>
-            <a class="nav-link link-inscription" href="register.php">S'inscrire</a>
+            <?php if($title !== 'Dashboard Demenageur'): ?>
+                <a class="nav-link link-connection" href="login.inc.php">Se connecter</a>
+                <a class="nav-link link-inscription" href="register.php">S'inscrire</a>
+            <?php else: ?>
+                <div class="rocket">
+                    <i class="fa-solid fa-rocket"></i>
+                </div>
+                <div class="inbox">
+                    <i class="fa-solid fa-inbox"></i>
+                </div>
+                <div class="profil-user">
+                    <span>Profile</span>
+                    <i class="fa-solid fa-user"></i>
+                </div>
+            <?php endif; ?>
             <div class="bar-diviseur-vertical"></div>
             <i class="fa-solid fa-cloud-moon"></i>
         </div>
+        <?php if($title !== 'Dashboard Demenageur'): ?>
         <div class="menu-icon">
             <i class="fa-solid fa-bars"></i>
             <span>Menu</span>
         </div>
+        <?php endif; ?>
     </div>
 </nav>
 
