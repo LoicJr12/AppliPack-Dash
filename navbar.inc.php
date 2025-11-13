edcfd246a2b3c78b11d19d06d9ad02efbed47f51
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-13 16:48:31 +0100

    mise a jour pour l'affichage sur demenageur.inc.php

--- navbar.inc.php
+++ navbar.inc.php
@@ -27,50 +27,80 @@
                 <a class="nav-link link-connection" href="login.inc.php">Se connecter</a>
                 <a class="nav-link link-inscription" href="register.php">S'inscrire</a>
             <?php else: ?>
-                <div class="rocket">
+                <a class="nav-link rocket" href="#">
                     <i class="fa-solid fa-rocket"></i>
-                </div>
-                <div class="inbox">
+                </a>
+                <a class="nav-link inbox" href="#">
                     <i class="fa-solid fa-inbox"></i>
-                </div>
-                <div class="profil-user">
+                </a>
+                <a class="nav-link inbox" href="#">
+                    <i class="fa-solid fa-headset"></i>
+                </a>
+                <a class="nav-link profil-user" href="#">
                     <span>Profile</span>
                     <i class="fa-solid fa-user"></i>
-                </div>
+                </a>
             <?php endif; ?>
             <div class="bar-diviseur-vertical"></div>
             <i class="fa-solid fa-cloud-moon"></i>
         </div>
-        <?php if($title !== 'Dashboard Demenageur'): ?>
         <div class="menu-icon">
             <i class="fa-solid fa-bars"></i>
             <span>Menu</span>
         </div>
-        <?php endif; ?>
     </div>
 </nav>
 
 <div class="menu-responsive">
     <div class="listLink-menu">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#infos">Infos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#slogan">A propos de nous</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#contacts">Contacter nous</a>
-            </li>
+            <?php if($title !== 'Dashboard Demenageur'): ?>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#infos">Infos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#slogan">A propos de nous</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contacts">Contacter nous</a>
+                </li>
+            <?php else: ?>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#infos">Nouvelles annonces</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Mes propositions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                        <i class="fa-solid fa-headset"></i>
+                        <span>Service client</span>
+                    </a>
+                </li>
+            <?php endif ?>
         </ul>
     </div>
     <div class="bar-diviseur-horizontal"></div>
     <div class="list-Buttons-menu">
-        <a class="nav-link link-connection" href="login.inc.php">Se connecter</a>
-        <a class="nav-link link-inscription" href="register.php">S'inscrire</a>
+        <?php if($title !== 'Dashboard Demenageur'): ?>
+            <a class="nav-link link-connection" href="login.inc.php">Se connecter</a>
+            <a class="nav-link link-inscription" href="register.php">S'inscrire</a>
+        <?php else: ?>
+            <a class="nav-link profil-user" href="#">
+                <span>Profile</span>
+                <i class="fa-solid fa-user"></i>
+            </a>
+            <a class="nav-link deconnexion bg-danger" href="#">
+                <i class="fa-solid fa-right-from-bracket"></i>
+                <span>Deconnexion</span>
+            </a>
+        <?php endif ?>
         <a class="nav-link link-dark-mode bg-dark" href="#">
             <span>Dark mode</span>
             <i class="fa-solid fa-moon"></i>
