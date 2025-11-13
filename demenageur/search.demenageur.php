d07886e7db96c4fc311c74b0c31fc5b97b8c0a8e
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-13 18:07:39 +0100

    mise a jour pour l'affichage sur demenageur.inc.php

---
+++ demenageur/search.demenageur.php
@@ -0,0 +1,10 @@
+<form action="" method="post" class="mt-3 mb-3">
+    <div class="container-fluid containeur-search">
+        <div class="SearchBar">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <div class="input-bar">
+                <input name="Search" class="form-control" id="search" type="text" placeholder="Enter le nom de la ville">
+            </div>
+        </div>
+    </div>
+</form>
