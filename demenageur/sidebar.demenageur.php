237abea99115999fa1873dcfda336474eff83715
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 10:20:25 +0100

    update

--- demenageur/sidebar.demenageur.php
+++ demenageur/sidebar.demenageur.php
@@ -1,54 +1,52 @@
 <div class="sidebar">
-    <div class="col-sm-5">
-        <div class="head-sidebar">
-            <h3>Welcome <?php echo $lastName ?></h3>
-            <button class="toggle"><i class="fa-solid fa-less-than"></i></button>
-        </div>
-        <nav class="nav-sidebar">
-            <div class="listLink-menu">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa-solid fa-house"></i>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa-solid fa-rocket"></i>
-                            Nouvelles annonces
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa-solid fa-inbox"></i>
-                            Mes propositions
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa-solid fa-user"></i>
-                            Mon profile
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="listLink-menu">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa-solid fa-moon"></i>
-                            Dark mode
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa-solid fa-right-from-bracket"></i> 
-                            Deconnexion
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <div class="head-sidebar">
+        <h3>Welcome <?php echo $lastName ?></h3>
+        <button class="toggle"><i class="fa-solid fa-less-than"></i></button>
     </div>
+    <nav class="nav-sidebar">
+        <div class="listLink-menu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-house"></i>
+                        Dashboard
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-rocket"></i>
+                        Nouvelles annonces
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-inbox"></i>
+                        Mes propositions
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-user"></i>
+                        Mon profile
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="listLink-menu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-moon"></i>
+                        Dark mode
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa-solid fa-right-from-bracket"></i> 
+                        Deconnexion
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </div>
