9225f2868994267d110f78d7d158e5a26a591abe
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 11:00:16 +0100

    update

--- demenageur.inc.php
+++ demenageur.inc.php
@@ -3,10 +3,18 @@
             include('header.inc.php');
             include('navbar.inc.php');
             include('demenageur/search.demenageur.php');
-            $lastName  = 'Loïc :)';
-            include('demenageur/sidebar.demenageur.php');
-            include('footer.inc.php');
         ?>
+        <div class="col">
+            <div>
+                <?php
+                    $lastName = 'Loïc :)';
+                    include('demenageur/sidebar.demenageur.php');
+                ?>
+            </div>
+            <div>
+                <?php include('footer.inc.php');?>
+            </div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <script src="styles/animation.menu.js"></script>
     </body>
