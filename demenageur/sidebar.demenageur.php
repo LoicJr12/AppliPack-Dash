36798168e23746ff7e674ddb29903cc7be50a727
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 09:48:36 +0100

    ajout du contenu

---
+++ demenageur/sidebar.demenageur.php
@@ -0,0 +1,36 @@
+<div class="sidebar">
+    <div class="container-sm">
+        <div class="head-sidebar">
+            <h3>Welcome <?php echo $lastName ?></h3>
+            <button class="toggle"></button>
+        </div>
+        <nav class="nav-sidebar">
+            <div class="listLink-menu">
+                <ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nouvelles annonces</a>
+                    /li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mes propositions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="listLink-menu">
+                <ul>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Service client</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Deconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
