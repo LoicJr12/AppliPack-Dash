2ee083970bd435b6c7037859c1a05af9e3ef2e55
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 15:35:59 +0100

    update

--- logout.inc.php
+++ logout.inc.php
@@ -1,9 +1,7 @@
 <?php 
-    if (session_status() === PHP_SESSION_ACTIVE) {
-        session_destroy();
-        header("Location: login.inc.php");
-        exit();  
-    } else {
-        echo "aUCUNE SESSION EN COUR";
-    }
+    session_start();
+    $_SESSION = array();
+    session_destroy();
+    header("Location: login.inc.php");
+    exit();  
 ?>
