469200f8202bf17fd0fabc9d4ae66d0dbc82b5bd
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 13:34:46 +0100

    mise à jour

--- formProposition.inc.php
+++ formProposition.inc.php
@@ -1,9 +1,24 @@
-<form action="" method="post">
+<form action="" method="get" class="new-proposition" style="width:500px;">
     <div class="head-form mb-3">
         <h3 class="auth-text" style="color : " >Nouvelle Proposition</h3>
         <span class="text-body-secondary">Remplissez les champs ci dessous</span>
     </div>
     <div class="field">
-
+        <div class="input-group mb-3">
+            <label for="prixproposition" class="form-label">Prix</label>
+            <div class="input-group">
+                <span class="input-group-text">$</span>
+                <input type="text" class="form-control" id="prixproposition" name="prix">
+                <span class="input-group-text">.00</span>
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="messageInput" class="form-label">Message</label>
+            <textarea class="form-control" id="messageInput" name="messagePropositions" rows="2"></textarea>
+        </div>
+        <div class="d-flex f-row buttonForm">
+            <button type="submit" class="btn btn-primary">Soummettre</button> 
+            <button type="reset" class="btn btn-danger">Renitialiser</button>
+        </div>
     </div>
 </form>
