791ce821b1175e32cddd51e92ddb9c2766b49a1c
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 17:56:48 +0100

    update

--- demenageur/annonce.demenageur.php
+++ demenageur/annonce.demenageur.php
@@ -8,12 +8,19 @@
         $bdd = new PDO("mysql:host=$servername;dbname=pack&dash", $username, $password);
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql = "SELECT * FROM annonce";
+        $sql = "SELECT * FROM annonce ORDER BY date_de_publication DESC";
         $request = $bdd->prepare($sql);
         $request->execute();
+        $listAnnonce = array();
         while($annonce = $request->fetch(PDO::FETCH_ASSOC)){
-            $listAnnoce[] = $annonce ;
+            $listAnnonce[] = $annonce ;
         }
+
+        // Affichage des annonces triées
+        foreach ($listAnnonce as $annonce) {
+            echo $annonce['idAnnonce'] . ' - ' . $annonce['titre'] . ' - ' . $annonce['date_de_publication'] . '<br>';
+        }
+
     } catch (PDOException $e) {
         echo "Erreur : " . $e->getMessage();
     }
