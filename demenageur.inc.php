691cd0671fcecfb85301bdb5f1e3ff55867576d9
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 10:11:21 +0100

    update

--- demenageur.inc.php
+++ demenageur.inc.php
@@ -3,7 +3,8 @@
             include('header.inc.php');
             include('navbar.inc.php');
             include('demenageur/search.demenageur.php');
-            include('demenageur/side.demenageur.php');
+            $lastName  = 'Loïc :)';
+            include('demenageur/sidebar.demenageur.php');
             include('footer.inc.php');
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
