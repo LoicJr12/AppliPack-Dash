ff2cc11491b7e068d320b709090f8d09b09acb6e
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 12:01:45 +0100

    update

---
+++ formProposition.inc.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+    <div class="head-form mb-3">
+        <h3 class="auth-text" style="color : " >Nouvelle Proposition</h3>
+        <span class="text-body-secondary">Remplissez les champs ci dessous</span>
+    </div>
+    <div class="field">
+
+    </div>
+</form>
