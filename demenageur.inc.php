cbc75274d929b5a81fa9e49bd5a964caa965fd39
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-14 13:39:17 +0100

    changement de la disposition

--- demenageur.inc.php
+++ demenageur.inc.php
@@ -5,16 +5,28 @@
             include('demenageur/search.demenageur.php');
         ?>
         <div class="col">
-            <div>
-                <?php
-                    $lastName = 'Loïc :)';
-                    include('demenageur/sidebar.demenageur.php');
-                ?>
+            <div class="row">
+                <div class="col-md-3">
+                    <?php
+                        $lastName = 'Loïc :)';
+                        include('demenageur/sidebar.demenageur.php');
+                    ?>
+                </div>
+                <div class="col-md-9">
+                    <?php include('formProposition.inc.php'); ?>
+                </div>
             </div>
             <div>
                 <?php include('footer.inc.php');?>
             </div>
         </div>
+        <style>
+            @media (max-width : 992px) {
+                .col-md-3{
+                    display: none;
+                }
+            }
+        </style>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <script src="styles/animation.menu.js"></script>
     </body>
