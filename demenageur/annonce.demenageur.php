74811d1b505aebf5c080779148a2655ee5c45b21
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-15 20:10:27 +0100

    update

--- demenageur/annonce.demenageur.php
+++ demenageur/annonce.demenageur.php
@@ -1,6 +1,4 @@
 <?php
-    session_start();
-
     try {
         $servername = 'localhost';
         $username = 'root';
@@ -16,14 +14,42 @@
             $listAnnonce[] = $annonce ;
         }
 
-        // Affichage des annonces triées
-        foreach ($listAnnonce as $annonce) {
-            echo $annonce['idAnnonce'] . ' - ' . $annonce['titre'] . ' - ' . $annonce['date_de_publication'] . '<br>';
-        }
-
     } catch (PDOException $e) {
         echo "Erreur : " . $e->getMessage();
     }
-
-    
 ?>
+
+<?php foreach($listAnnonce as $annonce) { ?>
+    <div class="card mb-3">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo htmlspecialchars($annonce['titre']); ?></h5>
+            <p class="card-text"><?php echo htmlspecialchars($annonce['description']); ?></p>
+            <p class="card-text"><strong>Nombre de demenageurs :</strong> <?php echo htmlspecialchars($annonce['nbreDemenageur']); ?></p>
+            <p class="card-text"><strong>Surface :</strong> <?php echo htmlspecialchars($annonce['volumeTotal']); ?></p>
+            <p class="card-text">
+                <strong>Date et heure du déménagement :</strong> 
+                <?php echo htmlspecialchars($annonce['date']).' à '.htmlspecialchars($annonce['heure']); ?>
+            </p>
+            <p class="card-text"><small class="text-muted">Publié le : <?php echo htmlspecialchars($annonce['date_de_publication']); ?></small></p>
+            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+                Faire une proposition 📝
+            </button>
+        </div>
+    </div>
+<?php } ?>
+
+<div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="staticBackdropLabel">Nouvelle Proposition</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div class="mb-2">
+        <strong>Alors <?php echo htmlspecialchars($lastName); ?> 💁‍♂️</strong>
+        <p>Que proposez vous ? Le client n'attend plus que vous. ⏳</p>
+    </div>
+    <div>
+        <?php include('formProposition.inc.php'); ?>
+    </div>
+  </div>
+</div>
