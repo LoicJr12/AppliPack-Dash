39f38e5fad73360d3b5696f6c3346c682fc32783
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-16 02:40:01 +0100

    update affichage des annonces

--- demenageur/annonce.demenageur.php
+++ demenageur/annonce.demenageur.php
@@ -19,24 +19,26 @@
     }
 ?>
 
-<?php foreach($listAnnonce as $annonce) { ?>
-    <div class="card mb-3 bg-light w-90">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo htmlspecialchars($annonce['titre']); ?></h5>
-            <p class="card-text"><?php echo htmlspecialchars($annonce['description']); ?></p>
-            <p class="card-text"><strong>Nombre de demenageurs :</strong> <?php echo htmlspecialchars($annonce['nbreDemenageur']); ?></p>
-            <p class="card-text"><strong>Surface :</strong> <?php echo htmlspecialchars($annonce['volumeTotal']); ?></p>
-            <p class="card-text">
-                <strong>Date et heure du déménagement :</strong> 
-                <?php echo htmlspecialchars($annonce['date']).' à '.htmlspecialchars($annonce['heure']); ?>
-            </p>
-            <p class="card-text"><small class="text-muted">Publié le : <?php echo htmlspecialchars($annonce['date_de_publication']); ?></small></p>
-            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-                Faire une proposition 📝
-            </button>
+<div class="displayCard">
+    <?php foreach($listAnnonce as $annonce) { ?>
+        <div class="card mb-3 bg-light">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo htmlspecialchars($annonce['titre']); ?></h5>
+                <p class="card-text"><?php echo htmlspecialchars($annonce['description']); ?></p>
+                <p class="card-text"><strong>Nombre de demenageurs :</strong> <?php echo htmlspecialchars($annonce['nbreDemenageur']); ?></p>
+                <p class="card-text"><strong>Surface :</strong> <?php echo htmlspecialchars($annonce['volumeTotal']); ?></p>
+                <p class="card-text">
+                    <strong>Date et heure du déménagement :</strong> 
+                    <?php echo htmlspecialchars($annonce['date']).' à '.htmlspecialchars($annonce['heure']); ?>
+                </p>
+                <p class="card-text"><small class="text-muted">Publié le : <?php echo htmlspecialchars($annonce['date_de_publication']); ?></small></p>
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+                    Faire une proposition 📝
+                </button>
+            </div>
         </div>
-    </div>
-<?php } ?>
+    <?php } ?>
+</div>
 
 <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
   <div class="offcanvas-header">
