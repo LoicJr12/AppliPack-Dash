0865c72fb1c29bec3d948adf7d3f88f17ee6b724
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-16 02:25:08 +0100

    mise a jour de la disposition

--- demenageur.inc.php
+++ demenageur.inc.php
@@ -1,20 +1,20 @@
         <?php
-            session_start();
+            //session_start();
 
             //Vérifier si l'utilisateur est connecté
-            if (!isset($_SESSION['idUtilisateur'])) {
-                header("Location: login.inc.php");
-                exit();
-            }
+            //if (!isset($_SESSION['idUtilisateur'])) {
+            //    header("Location: login.inc.php");
+            //    exit();
+            //}
 
             $title = 'Dashboard Demenageur';
             include('header.inc.php');
             include('navbar.inc.php');
             include('demenageur/search.demenageur.php');
         ?>
-        <div class="col">
-            <div class="row">
-                <div class="col-md-3">
+        <div class="col-md-12 main-content">
+            <div class="row gx-4">
+                <div class="sidebar-section">
                     <?php
                         $lastName = "Loïc";
                         if(isset($_SESSION['userName'])){
@@ -25,7 +25,7 @@
                 </div>
                 <div class="col-md-5 annonces-section">
                     <div class="mb-2 mt-2">
-                        <h4>🚀 Nouvelles Annonces</h3>
+                        <h4>🚀 Nouvelles Annonces</h>
                     </div>
                     <?php include('demenageur/annonce.demenageur.php'); ?>
                 </div>
@@ -40,14 +40,8 @@
                 <?php include('footer.inc.php');?>
             </div>
         </div>
-        <style>
-            @media (max-width : 992px) {
-                .col-md-3{
-                    display: none;
-                }
-            }
-        </style>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <script src="styles/animation.menu.js"></script>
+        <!--- <script src="styles/animation.message.js"></script> -->
     </body>
 </html>
