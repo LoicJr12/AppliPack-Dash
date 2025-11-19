59c9ef4d521b01009117983be398721fa712587f
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-19 17:35:29 +0100

    update

--- demenageur/annonce.demenageur.php
+++ demenageur/annonce.demenageur.php
@@ -79,10 +79,9 @@
                     data-id="<?php echo $annonce['idAnnonce']; ?>" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
                     Faire une proposition 📝
                 </button>
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1" 
-                    data-id="<?php echo htmlspecialchars($annonce['idAnnonce']); ?>">
+                <a href="demenageur/voirDetailsAnnonce.php?ref=<?php echo htmlspecialchars($annonce['idAnnonce']); ?>" class="btn btn-primary" >
                     voir details
-                </button>
+                </a>
             </div>
         </div>
     <?php } ?>
@@ -100,65 +99,6 @@
     </div>
     <div>
         <?php include('formProposition.inc.php'); ?>
-    </div>
-  </div>
-</div>
-
-<!-- Modal -->
- <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel1" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content bg-light">
-      <div class="modal-header">
-        <h1 class="modal-title1 fs-5" id="exampleModalLabel1">Details de l'annonce</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <input type="text" class="form-control idAnnonceDetails" id="recipient-name" value="">
-        <?php
-            $idAnnonceRecuperer = $_GET['refDetails'];
-            if(isset($idAnnonceRecuperer)) :
-                foreach($listAnnonce as $annonce):
-                    if($annonce['idAnnonce'] === $idAnnonceRecuperer): 
-        ?>
-            <div class="card mb-3 bg-light">
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo htmlspecialchars($annonce['titre']); ?></h5>
-                    <p class="card-text"><?php echo htmlspecialchars($annonce['description']); ?></p>
-                    <div class="d-flex flex-row gap-3">
-                        <p class="card-text"><strong>Nombre de demenageurs :</strong> <?php echo htmlspecialchars($annonce['nbreDemenageur']); ?></p>
-                        <p class="card-text"><strong>Surface :</strong> <?php echo htmlspecialchars($annonce['volumeTotal']); ?> m²</p>
-                    </div>
-                    <div class="d-flex flex-row gap-3">
-                        <?php 
-                            foreach($listLogemementDepart as $logementDepart) :
-                                if($logementDepart['idAnnonce'] === $annonce['idAnnonce']):
-                        ?>
-                            <p class="card-text"><strong>Depart :</strong> <?php echo htmlspecialchars($logementDepart['villeDepart']); ?></p>
-                        <?php endif ;
-                            endforeach; 
-                        ?>
-                        <?php 
-                            foreach($listLogemementArrivee as $logementArrivee) :
-                                if($logementArrivee['idAnnonce'] === $annonce['idAnnonce']):
-                        ?>
-                            <p class="card-text"><strong>Arrivee :</strong> <?php echo htmlspecialchars($logementArrivee['villeArrivee']); ?></p>
-                        <?php endif ;
-                            endforeach; 
-                        ?>
-                    </div>
-                    <p class="card-text">
-                        <strong>Date et heure du déménagement :</strong> 
-                        <?php echo htmlspecialchars($annonce['date']).' à '.htmlspecialchars($annonce['heure']); ?>
-                    </p>
-                    <p class="card-text"><small class="text-muted">Publié le : <?php echo htmlspecialchars($annonce['date_de_publication']); ?></small></p>
-                </div>
-            </div>
-        <?php 
-                    endif;
-                endforeach; 
-            endif;
-        ?>
-      </div>
     </div>
   </div>
 </div>
