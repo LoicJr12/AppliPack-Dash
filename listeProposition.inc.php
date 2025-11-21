0ec728e8c6546cebf79bb530e32cbe5f7388de3b
KAKOU <goue.kakou@groupe-esigelec.org>
2025-11-21 11:34:20 +0100

    ajout de la fonctionnalité message

--- listeProposition.inc.php
+++ listeProposition.inc.php
@@ -88,20 +88,20 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
+        <form action="traitementMessage.php" method="post">
           <div class="mb-3">
             <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name" readonly>
+            <input type="text" class="form-control" id="recipient-name" name="recepteur" readonly>
           </div>
           <div class="mb-3">
             <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
+            <textarea class="form-control" id="message-text" name="message"></textarea>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Send message</button>
           </div>
         </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send message</button>
       </div>
     </div>
   </div>
